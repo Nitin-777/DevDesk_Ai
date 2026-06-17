494f5396d1f8d67cd2581ac95f414930f3824aab
Nitin <nitinsharmaop828@gmail.com>
2026-06-17 15:01:27 +0530

    ticke_api

---
+++ client/src/api/ticketApi.js
@@ -0,0 +1,21 @@
+import api from "./axios";
+
+export const createTicket = async (ticketData) => {
+  const { data } = await api.post("/tickets", ticketData);
+  return data.ticket;
+};
+
+export const getMyTickets = async () => {
+  const { data } = await api.get("/tickets/my");
+  return data.tickets;
+};
+
+export const getTicketById = async (ticketId) => {
+  const { data } = await api.get(`/tickets/${ticketId}`);
+  return data.ticket;
+};
+
+export const addTicketReply = async (ticketId, replyData) => {
+  const { data } = await api.post(`/tickets/${ticketId}/replies`, replyData);
+  return data.ticket;
+};
