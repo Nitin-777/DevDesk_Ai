150b7cf8e662fb7f9be2120f8fb05f12c5b19738
Nitin <nitinsharmaop828@gmail.com>
2026-06-18 20:16:43 +0530

    admin api.js

---
+++ client/src/api/adminApi.js
@@ -0,0 +1,21 @@
+import api from "./axios";
+
+export const getOverviewStats = async () => {
+  const { data } = await api.get("/analytics/overview");
+  return data.stats;
+};
+
+export const getAllTickets = async () => {
+  const { data } = await api.get("/tickets/all");
+  return data.tickets;
+};
+
+export const getAgents = async () => {
+  const { data } = await api.get("/users/agents");
+  return data.agents;
+};
+
+export const assignTicket = async ({ ticketId, agentId }) => {
+  const { data } = await api.patch(`/tickets/${ticketId}/assign`, { agentId });
+  return data.ticket;
+};
